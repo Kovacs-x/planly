ac4e4b530f1f2929dcb2a37b44c38da7e0a45e33
Kovacs-x <mustafa.moham21@gmail.com>
2026-09-22 10:25:24 +0100

    Cache Planly 2.2 bundle

--- v2/sw.js
+++ v2/sw.js
@@ -1,5 +1,5 @@
-const CACHE='planly-v2-clean-3';
-const ASSETS=['./','./index.html','./app-v2.1.0.js','./manifest.webmanifest','../icon-192.png','../icon-512.png'];
+const CACHE='planly-v2-clean-4';
+const ASSETS=['./','./index.html','./app-v2.2.0.js','./manifest.webmanifest','../icon-192.png','../icon-512.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
