3d503d3360ab73fb1c712b31037ab93fcfb85448
Kovacs-x <mustafa.moham21@gmail.com>
2026-09-22 06:19:44 +0100

    Add Planly v2 service worker

---
+++ v2/sw.js
@@ -0,0 +1,31 @@
+const CACHE='planly-v2-clean-1';
+const ASSETS=['./','./index.html','./app.js?v=2.0.0','./manifest.webmanifest','../icon-192.png','../icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key.startsWith('planly-v2-')&&key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(
+    fetch(event.request,{cache:'no-store'})
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
+  );
+});
