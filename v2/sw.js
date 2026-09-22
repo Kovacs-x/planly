fae7a690328f6a2e478f1c6fec828c95ed293b53
Kovacs-x <mustafa.moham21@gmail.com>
2026-09-22 06:32:06 +0100

    Bump Planly v2 cache to 2.0.1

--- v2/sw.js
+++ v2/sw.js
@@ -1,5 +1,5 @@
-const CACHE='planly-v2-clean-1';
-const ASSETS=['./','./index.html','./app.js?v=2.0.0','./manifest.webmanifest','../icon-192.png','../icon-512.png'];
+const CACHE='planly-v2-clean-2';
+const ASSETS=['./','./index.html','./app.js?v=2.0.1','./manifest.webmanifest','../icon-192.png','../icon-512.png'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(
