0661fdf6afed77fad0f3826653dd0914acf6f50a
Kovacs-x <mustafa.moham21@gmail.com>
2026-09-24 05:38:05 +0100

    Add isolated Planly preview config

---
+++ v2-preview/supabase-config.js
@@ -0,0 +1,5 @@
+window.PLANLY_SUPABASE_CONFIG={
+  url:'https://dtniwcwjucepsjzoojuc.supabase.co',
+  publishableKey:'sb_publishable_RTHxTr4653cqVPGnSKlO2g_7e_5yLvx'
+};
+window.PLANLY_GOOGLE_CLIENT_ID='171071129565-49nuqh2l2i83ftg8gnu8evsfq3qh48r8.apps.googleusercontent.com';
