80c4f0945c7c0eeb32c675bcff7a6a9fb56c1ae1
Kovacs-x <mustafa.moham21@gmail.com>
2026-09-24 17:19:20 +0100

    Align service worker cache and probe version

--- v2/sw.js
+++ v2/sw.js
@@ -1,6 +1,6 @@
-const CACHE='planly-v2-330a3';
-const VERSION='planly-v2-sw-330a3';
-const APP_URL='./app-v3.2.0.js?v=330a3';
+const CACHE='planly-v2-330a4';
+const VERSION='planly-v2-sw-330a4';
+const APP_URL='./app-v3.2.0.js?v=330a4';
 const REQUIRED=['./index.html',APP_URL,'./supabase-config.js','./manifest.webmanifest'];
 const OPTIONAL=['./','../icon-192.png','../icon-512.png'];
 
